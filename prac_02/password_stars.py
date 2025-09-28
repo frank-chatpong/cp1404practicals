5f64fcdaa4b229746560e3b340677cb7ece8d651
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-09-28 20:33:59 +1000

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,24 +1,8 @@
 MINIMUM_LENGTH = 10
 
+"""Get a password of valid size and print asterisks."""
+password = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
+while len(password) < MINIMUM_LENGTH:
+    password = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
 
-def main():
-    """Get password and print asterisks """
-    password = get_password(MINIMUM_LENGTH)
-    print_asterisks(password)
-
-
-def get_password(password_length):
-    """Get password that meets the required length"""
-    password = input(f"Please enter the password (at least {password_length} characters): ")
-    while len(password) < password_length:
-        print("Error: Password too short!")
-        password = input(f"Please enter the password (at least {password_length} characters): ")
-    return password
-
-
-def print_asterisks(password):
-    """Print asterisks as long as the password"""
-    print('*' * len(password))
-
-
-main()
+print('*' * len(password))
