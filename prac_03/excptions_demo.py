0057abf113cab84bdb0c802d8eecc82e59caf4e1
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-10-05 22:34:30 +1000

    Edit code to avoid ZeroDivisionError

--- prac_03/excptions_demo.py
+++ prac_03/excptions_demo.py
@@ -9,12 +9,14 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
-    fraction = numerator / denominator
-    print(fraction)
+    if denominator == 0:
+        print("Cannot divide by zero!")
+    else:
+        fraction = numerator / denominator
+        print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
+
 print("Finished.")
 
 """
