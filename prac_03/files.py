080f0725d6eb5e1b350e5ace3c6536c003fcb3e8
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-10-05 23:02:41 +1000

    Answer question 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -13,3 +13,15 @@ out_file = open("name.txt", 'w')
 print(name, file=out_file)
 
 out_file.close()
+
+# 2. In the same file, but as if it were a separate program, write code that opens "name.txt"
+# and reads the name (as above) then prints (note the exact output),
+# Hi Bob! (or whatever the name is in the file). Do not simply print the user's input variable!
+# Use open and close for this question.
+
+"""Open the file and read the name"""
+in_file = open("name.txt", 'r')
+name = in_file.read().strip()
+in_file.close()
+
+print(f"Hi {name}!")
