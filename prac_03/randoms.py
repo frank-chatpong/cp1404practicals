2d1b870df1ad009f4a026dde78ef2a7fb5452106
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-10-05 21:42:16 +1000

    Add comment for random number between 1 and 100

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -18,4 +18,5 @@ Answer the questions about random
 # ANS : The smallest number that I can see is 2.6209650846534696 and the largest is 5.471547885591382
 
 import random
+"""Print a random number between 1 and 100 inclusive"""
 print(random.randint(1, 100))
