7f0f73b3ef82a02171c2fd49ef9ab3106cfa0ed9
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-10-05 21:28:20 +1000

    Answer the questions about randoms

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 - Practical
+Answer the questions about random
+"""
+
+# 1. What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# ANS : The smallest number that I can see is 5 and the largest is 20
+#
+# 2. What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# ANS : The smallest number that I can see is 3 and the largest is 9
+# The line did not produced the 4, it's only 3,5,7,9
+#
+# 3. What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# ANS : The smallest number that I can see is 2.6209650846534696 and the largest is 5.471547885591382
+
