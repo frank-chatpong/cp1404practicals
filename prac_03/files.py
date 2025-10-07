a137fc5243e34901b20cf67dced0c535885d6cae
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-10-07 12:54:26 +1000

    Edit 3 object name

--- prac_03/files.py
+++ prac_03/files.py
@@ -31,10 +31,10 @@ print(f"Hi {name}!")
 
 """Open file and calculate by using first 2 lines"""
 with open("numbers.txt", 'r') as in_file:
-    number1 = int(in_file.readline())
-    number2 = int(in_file.readline())
+    first_number = int(in_file.readline())
+    second_number = int(in_file.readline())
 
-print(number1 + number2)
+print(first_number + second_number)
 
 # 4. Now write a fourth block of code that prints the total for all lines in numbers.txt.
 # This should work for a file with any number of numbers.
