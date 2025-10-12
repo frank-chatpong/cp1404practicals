13a123a192c1d6db6b306ca1644c8389d81cffed
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-10-12 23:04:46 +1000

    Edit subject reader program by modify the load data function to return the data as a list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -12,18 +12,18 @@ def main():
 
 
 def load_data(filename=FILENAME):
-    """Read data from file formatted like: subject,lecturer,number of students."""
+    """Read data from file formatted like: subject,lecturer,number of students.
+    Return data as a list of [subject, lecturer, number_of_students].
+    """
+    data = []
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+        line = line.strip()              # Remove the \n
+        parts = line.split(',')          # Separate the data into its parts
+        parts[2] = int(parts[2])         # Make the number an integer (ignore PyCharm's warning)
+        data.append(parts)               # Add this list to the data list
+    input_file.close()                   # Close the file
+    return data
 
 
 main()
