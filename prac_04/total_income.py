1df0604fdf1fd6f5d342ed82a1449ad63d75d260
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-10-12 23:21:14 +1000

    Add comments on the print income report function in cumulative total income program practical 04

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -17,6 +17,7 @@ def main():
 
 
 def print_income_report(incomes):
+    """Print income report"""
     print("\nIncome Report\n-------------")
     total = 0
     for month, income in enumerate(incomes, 1):
