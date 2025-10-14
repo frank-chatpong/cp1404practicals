66425c479d544aec31dba6be22c07e6dd41cf9d2
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-10-14 12:36:30 +1000

    Change variable and function names in subject reader in practical 04

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,29 +8,30 @@ FILENAME = "subject_data.txt"
 
 def main():
     """Read subject data and display"""
-    data = load_subjects(FILENAME)
-    display_subjects(data)
+    subject_data = load_subjects(FILENAME)
+    display_subjects(subject_data)
 
 
 def load_subjects(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students.
     Return data as a list of [subject, lecturer, number_of_students].
     """
-    data = []
+    subject_data = []
     input_file = open(filename)
     for line in input_file:
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        data.append(parts)  # Add this list to the data list
+        subject_data.append(parts)  # Add this list to the data list
     input_file.close()  # Close the file
-    return data
+    return subject_data
 
 
-def display_subjects(data):
-    """Display subject, lecturer, and number_of_students from the data list."""
-    for subject, lecturer, number_of_students in data:
+def display_subjects(subject_data):
+    """Display subject, lecturer, and number_of_students from the subject data list."""
+    for subject, lecturer, number_of_students in subject_data:
         print(f"{subject} is taught by {lecturer} and has {number_of_students} students")
 
 
 main()
+
