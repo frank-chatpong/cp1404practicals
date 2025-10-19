7d9b7fee0f9aded50bd42c77dd7b62f6856e1bd0
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-10-19 22:57:41 +1000

    Complete Word Occurrences program in practical 5

--- prac_05/word_occurrenced.py
+++ prac_05/word_occurrenced.py
@@ -1,5 +1,19 @@
 """
 Word Occurrences
 Estimate: 25 minutes
-Actual:    minutes
+Actual:  23  minutes
 """
+
+word_to_count = {}
+text = input("Text: ").lower()
+words = text.split()
+for word in words:
+    if word in word_to_count:
+        word_to_count[word] += 1
+    else:
+        word_to_count[word] = 1
+
+max_length = max((len(word) for word in word_to_count))
+
+for word in sorted(word_to_count):
+    print(f"{word:{max_length}} : {word_to_count[word]}")
