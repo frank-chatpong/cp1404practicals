fd995ac7322b0ad494d8c677c6581f925a77d214
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-10-19 22:27:52 +1000

    created colour code in a dictionary practical 05

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 Practical
+Colour code in a dictionary
+"""
+
+COLOR_TO_CODES = {"ALICEBLUE": "#f0f8ff", "ANTIQUEWHITE": "#faebd7", "AQUA": "#00ffff", "BLACK": "#000000",
+                  "BLUE": "#0000ff", "CORAL": "#ff7f50", "CRIMSON": "#dc143c", "GOLD": "#ffd700", "GREEN": "#008000",
+                  "HOTPINK": "#ff69b4"}
+print(COLOR_TO_CODES)
+
+
+
