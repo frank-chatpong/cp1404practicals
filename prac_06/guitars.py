13869f6e4183eed188b979ee8892a7613cf24fdf
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-02 23:34:23 +1000

    Add main function and test with given code in Guitars program in practical 6

---
+++ prac_06/guitars.py
@@ -0,0 +1,27 @@
+"""
+CP1404/CP5632 Practical - Guitars program.
+Estimate: 30 minutes
+Actual:    minutes
+"""
+
+from guitar import Guitar
+
+
+def main():
+    """Store user-input guitars and then display them by using Guitar class."""
+    guitars = []
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+        else:
+            vintage_string = ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+main()
+
