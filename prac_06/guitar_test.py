316f4bee276234b9b099ea75976d6b758617aa39
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-02 23:00:13 +1000

    Add print to check is vintage function in Guitar test program in practical 6

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -16,4 +16,5 @@ def main():
     print(f"{gibson.name} get_age() - Expected 103. Got {gibson.get_age()}")
     print(f"{another.name} get_age() - Expected 12. Got {another.get_age()}")
 
-
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another.name} is_vintage() - Expected False. Got {another.is_vintage()}")
