92472f79699eb62246bc20bace1fe14e013bad52
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-02 22:58:57 +1000

    Add main function in Guitar test program in practical 6

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,15 @@
+"""
+CP1404/CP5632 Practical - Guitar test program.
+Estimate: 30 minutes
+Actual:    minutes
+"""
+
+from guitar import Guitar
+
+
+def main():
+    """Test the get_age() and is_vintage() methods of the Guitar class."""
+
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another = Guitar("Another Guitar", 2013)
+
