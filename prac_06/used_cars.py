dbe6494449f16562336809b5641851bf19d4831b
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-02 21:26:34 +1000

    Add limo objects in the used car class program in practical 6

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -14,5 +14,11 @@ def main():
     print(f"{my_car.name} has fuel: {my_car.fuel}")
     print(my_car)
 
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    limo.drive(115)
+    print(f"{limo.name} has fuel: {limo.fuel}")
+    print(limo)
+
 
 main()
