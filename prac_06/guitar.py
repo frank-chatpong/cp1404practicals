6adb3c33753f947d2732896f52adae4d809bb077
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-02 22:34:44 +1000

    Add Guitar class and initialise function in Guitar program in practical 6

---
+++ prac_06/guitar.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 Practical - Guitar program.
+Estimate: 30 minutes
+Actual:    minutes
+"""
+
+class Guitar:
+    """Represent a Guitar objects."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance.
+
+        name: string, guitar's name
+        year: int, the year that guitar is made
+        cost: float, guitar's price
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+
+
