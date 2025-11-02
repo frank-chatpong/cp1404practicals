8912ba326bc81f3e0821a217a4adb326f6c94559
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-02 22:09:52 +1000

    Add given code in languages program in practical 6

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,3 +2,17 @@
 Estimate: 20 minutes
 Actual:    minutes
 """
+
+from programming_language import ProgrammingLanguage
+
+
+def main():
+    """Testing class ProgrammingLanguage."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+
+
+main()
