e398f48fe5dcd210ced117a2bbfc2d6b55b87334
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-02 22:17:41 +1000

    Add java and c++ in the list and add the loop test in languages program in practical 6

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -11,8 +11,15 @@ def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    java = ProgrammingLanguage("Java", "Static", True, "1995")
+    c_plus_plus = ProgrammingLanguage("C++", "Static", True, "1983")
     print(python)
 
+    languages = [python, ruby, visual_basic, java, c_plus_plus]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
 
 
 main()
