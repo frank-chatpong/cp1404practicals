d51f14adc756e63fe3c92ce55442d07e73c968c7
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-02 21:26:00 +1000

    Add name object in the used car class program in practical 6

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,9 +9,9 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My Car", 180)
     my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
+    print(f"{my_car.name} has fuel: {my_car.fuel}")
     print(my_car)
 
 
