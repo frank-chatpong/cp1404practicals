bac593b32553a76437fb78d9ec1401c439e66dcc
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-02 22:43:12 +1000

    Add get age function and is the guitar is vintage function in Guitar program in practical 6

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,6 +3,8 @@ CP1404/CP5632 Practical - Guitar program.
 Estimate: 30 minutes
 Actual:    minutes
 """
+CURRENT_YEAR = 2025
+
 
 class Guitar:
     """Represent a Guitar objects."""
@@ -18,5 +20,16 @@ class Guitar:
         self.year = year
         self.cost = cost
 
+    def __str__(self):
+        """Return string representation of a Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Returns how old the guitar."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine if the guitar is vintage."""
+        return self.get_age() >= 50
 
 
