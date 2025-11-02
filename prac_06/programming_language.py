f39829c2f04091287a189fc08dd34c4a6c7f65b5
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-02 21:55:05 +1000

    Add initial function in class ProgrammingLanguage in ProgrammingLanguage class example in practical 6

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -6,3 +6,21 @@ Actual:    minutes
 
 class ProgrammingLanguage:
     """Represent information about a programming language."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a ProgrammingLanguage from the given values
+
+        name: language name
+        typing: Static or Dynamic
+        reflection: True if it can do reflection
+        year: starting year
+        """
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
+
+
+
