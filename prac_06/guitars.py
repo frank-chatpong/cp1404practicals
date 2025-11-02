2df9bf276ce79399441d51584972a3c297e0b330
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-02 23:36:35 +1000

    Add input guitar name and details in Guitars program in practical 6

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -11,6 +11,16 @@ def main():
     """Store user-input guitars and then display them by using Guitar class."""
     guitars = []
 
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.\n")
+        name = input("Name: ")
+
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
