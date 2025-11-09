4feef8991d54a2a88cc58a881fdbb1c13b8fb43e
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-10 00:24:50 +1000

    Add asking user to input new guitar info function in Guitars exercise in practical 07

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -14,6 +14,8 @@ def main():
     for guitar in guitars:
         print(guitar)
 
+    add_new_guitars(guitars)
+
     guitars.sort()
 
     print("\nSorted Guitars (oldest to newest):")
@@ -34,6 +36,22 @@ def load_guitars(filename):
         guitars.append(Guitar(name, year, cost))
     in_file.close()
     return guitars
+
+def add_new_guitars(guitars):
+    """Ask user to add new guitar to the list."""
+    print("\nAdd new guitars (leave it blank to finish):")
+    name = input("Name: ").strip()
+    while name != "":
+        try:
+            year = int(input("Year: "))
+            cost = float(input("Cost: $"))
+        except ValueError:
+            print("Invalid input! Please enter number for year and cost.")
+        else:
+            guitar = Guitar(name, year, cost)
+            guitars.append(guitar)
+            print(f"{guitar} added.")
+        name = input("Name: ").strip()
 
 
 main()
