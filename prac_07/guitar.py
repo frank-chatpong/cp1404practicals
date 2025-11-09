1f6f32615de5c976ce20d8a61e8dc295f263c31e
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-09 23:53:35 +1000

    Add compare function in guitar file in practical 07

---
+++ prac_07/guitar.py
@@ -0,0 +1,39 @@
+"""
+CP1404/CP5632 Practical - Guitar program.
+Estimate: 30 minutes
+Actual:  30  minutes
+"""
+CURRENT_YEAR = 2025
+
+
+class Guitar:
+    """Represent a Guitar objects."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance.
+
+        name: string, guitar's name
+        year: int, the year that guitar is made
+        cost: float, guitar's price
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return string representation of a Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Returns how old the guitar."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine if the guitar is vintage."""
+        return self.get_age() >= 50
+
+    def __lt__(self, other):
+        """Compare guitars by year for sorting."""
+        return self.year < other.year
+
+
