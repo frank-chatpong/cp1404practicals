227c9da84912097d6a09fa0c7d30bc45b1322f8a
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-16 21:29:40 +1000

    Add draft kv layout to convert_miles_km.kv program exercise in practical 08

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,11 @@
+"""
+CP1404/CP5632 Practical
+Convert miles to km program
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+
+class ConvertMilesToKmApp(app):
+    """Convert miles to km is a Kivy App for converting miles to km."""
+    def
