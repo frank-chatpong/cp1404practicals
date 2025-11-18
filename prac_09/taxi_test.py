d88d69ae81ce6d9b25f207b42a848cc579334340
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-18 13:04:18 +1000

    Create a new taxi object in taxi test files in practical 09

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,3 +2,9 @@
 CP1404/CP5632 Practical - Client code to use the Car and Taxi class.
 """
 
+from prac_09.taxi import Taxi
+
+
+def main():
+    """Demo test code to show how to use Taxi class."""
+   my_taxi = Taxi("Prius", 100, 1.23)
