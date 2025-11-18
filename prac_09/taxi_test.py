14764e0a79399e809cc8015208f44f8f1740b03f
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-18 13:38:29 +1000

    Add restarting the meter in  taxi test files in practical 09

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,4 +7,26 @@ from prac_09.taxi import Taxi
 
 def main():
     """Demo test code to show how to use Taxi class."""
-   my_taxi = Taxi("Prius", 100, 1.23)
+    # my_taxi = Taxi("Prius 1", 100, 1.23)
+    # my_taxi.drive(40)
+    # print(my_taxi)
+    # my_taxi.start_fare()
+    # my_taxi.drive(100)
+    # print(my_taxi)
+
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+    print(
+        f"{my_taxi.name} has fuel: {my_taxi.fuel} and driven: {my_taxi.current_fare_distance}km, "
+        f"the current fare: {my_taxi.get_fare()}")
+
+    print("<<<Restart meter>>>")
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+    print(f"{my_taxi.name} has fuel: {my_taxi.fuel} and driven: {my_taxi.current_fare_distance}km,"
+          f"the current fare: {my_taxi.get_fare()}")
+
+
+main()
