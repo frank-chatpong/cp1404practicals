abae720bd815ab39ec7d032fcd424c27947b0d87
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-24 01:31:47 +1000

    Edit MENU text in taxi simulator program in practical 09

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -6,7 +6,7 @@ Taxi Simulator
 from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
-MENU = "q)uit, c)hoose taxi, d)rive\n"
+MENU = "q)uit, c)hoose taxi, d)rive\n>>> "
 
 
 def main():
