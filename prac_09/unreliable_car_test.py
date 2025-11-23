48367b9d5d00f2d49926817883384b31358061db
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-23 23:41:25 +1000

    Edit the loop in unreliable car test program in practical 09

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -13,7 +13,7 @@ def main():
     for i in range(1, 30):
         print(f"Attempting to drive {i} km.")
         print(f"{reliable_car.name:12} drove {reliable_car.drive(i):2} km")
-        print(f"{unreliable_car.name:12} drove {unreliable_car.drive:2} km")
+        print(f"{unreliable_car.name:12} drove {unreliable_car.drive(i):2} km")
     print(reliable_car)
     print(unreliable_car)
 
