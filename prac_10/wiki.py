3e7dca943d9a3e14caac0d8afce75b09f01feafc
frank-chatpong <chatpong.tahaprom@my.jcu.edu.au>
2025-11-30 22:50:57 +1000

    Add main function in wiki program in practical 10

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -6,5 +6,23 @@ import wikipedia
 
 
 def main():
+    """Run the Wikipedia search loop until the user enters blank input."""
     wikipedia.set_lang("en")
     title = input("Enter page title: ").strip()
+    while title != "":
+        try:
+            page = wikipedia.page(title)
+        except wikipedia.DisambiguationError as error:
+            print("We need a more specific title. Try one of the following, or a new search:")
+            print(error.options)
+        except wikipedia.PageError as error:
+            print(error)
+        else:
+            print_page_info(page)
+
+        print()
+        title = input("Enter page title: ").strip()
+
+ 
+
+
